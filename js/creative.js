da21550fa7b4e05566cb8cd35c8af77eff79705d
Sure-Fi Inc <sure-fi@Sure-Fis-iMac.local>
2018-01-18 13:18:49 -0700

    added carouse for the parterns

--- js/creative.js
+++ js/creative.js
@@ -92,4 +92,36 @@
     }
   });
 
+
+ var owl = $(".owl-carousel").owlCarousel(
+      {
+        nav : true,
+        navText: ['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],
+        autoplay : false,
+        autoplayTimeout:3000,
+        autoplaySpeed: 200,
+        loop: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:3, 
+            },
+            992:{
+                items:6,
+            }
+        }
+      });
+
+  $('#back-button').click(function() {
+      owl.trigger('next.owl.carousel');
+  })
+  // Go to the previous item
+  $('#front-button').click(function() {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      owl.trigger('prev.owl.carousel', [300]);
+  })
+
 })(jQuery); // End of use strict
