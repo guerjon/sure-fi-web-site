829facf43131f8b22bb6a8cccfe5a49fa79b2674
Jonathan Guerrero <guerjon@gmail.com>
2018-03-29 09:47:55 -0600

    added google analitics

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@
   gtag('config', 'UA-113437818-1');
 
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116619225-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116619225-1');
+</script>
+
