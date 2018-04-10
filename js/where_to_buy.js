e6a57fc13f31d2aeeea154ca742ffd2d1979a187
Jonathan Guerrero <guerjon@gmail.com>
2018-04-10 16:45:40 -0600

    partners thing

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -1,3 +1,21 @@
+if ( !window.requestAnimationFrame ) {
+
+  window.requestAnimationFrame = ( function() {
+
+    return window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element ) {
+
+      window.setTimeout( callback, 1000 / 60 );
+
+    };
+
+  } )();
+
+}
+
 var scroll_positions = [5,252,494,747,987]
 var rand = scroll_positions[Math.floor(Math.random() * scroll_positions.length)];
 
