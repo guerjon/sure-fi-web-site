6abc5242c10c2b8392d4cf3fdd76e0409e8fa574
Jonathan Guerrero <guerjon@gmail.com>
2018-04-10 11:00:32 -0600

    working on the partners site

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -1,10 +1,15 @@
+
+var myArray = [0, 1, 2,3,4];   
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+
 $(document).ready(function(){
   $('.vertical-carousel').slick({
     vertical: true,
     verticalSwiping: true,
     slidesToShow: 2,
     slidesToMove: 1,
-    arrows: false
+    arrows: false,
+    initialSlide : rand
   });
 });
 
