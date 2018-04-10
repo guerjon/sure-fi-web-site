fca90715368bbe0ef7a12d92657463d62474c036
Jonathan Guerrero <guerjon@gmail.com>
2018-04-10 16:55:19 -0600

    partners thing

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -81,7 +81,7 @@ function scrollUpdate () {
     // Disable scroll-jumping for a short time to avoid flickering
     window.setTimeout(function () {
       disableScroll = false;
-    }, 40);
+    }, 800);
   }
 }
 
