b3a5c2b5805a6d223253db8ca6b92bbd8274da88
Jonathan Guerrero <guerjon@gmail.com>
2018-04-10 15:58:51 -0600

    partners thing

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -1,17 +1,76 @@
+var doc = window.document,
+  context = doc.querySelector('.js-loop'),
+  clones = context.querySelectorAll('.is-clone'),
+  disableScroll = false,
+  scrollHeight = 0,
+  scrollPos = 3,
+  clonesHeight = 0,
+  i = 0;
 
-var myArray = [0, 1, 2,3,4];   
-var rand = myArray[Math.floor(Math.random() * myArray.length)];
+function getScrollPos () {
+  return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
+}
 
-$(document).ready(function(){
-  $('.vertical-carousel').slick({
-    vertical: true,
-    verticalSwiping: true,
-    slidesToShow: 2,
-    slidesToMove: 1,
-    arrows: false,
-    initialSlide : rand,
-    respondTo: 'slider'
-  });
-});
+function setScrollPos (pos) {
+  context.scrollTop = pos;
+}
+
+function getClonesHeight () {
+  clonesHeight = 0;
+
+  for (i = 0; i < clones.length; i += 1) {
+    clonesHeight = clonesHeight + clones[i].offsetHeight;
+  }
+
+  return clonesHeight;
+}
+
+function reCalc () {
+  scrollPos = getScrollPos();
+  scrollHeight = context.scrollHeight;
+  clonesHeight = getClonesHeight();
+
+  if (scrollPos <= 0) {
+    setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
+  }
+}
+
+function scrollUpdate () {
+  if (!disableScroll) {
+    scrollPos = getScrollPos();
+
+    if (clonesHeight + scrollPos >= scrollHeight) {
+      // Scroll to the top when you’ve reached the bottom
+      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
+      disableScroll = true;
+    } else if (scrollPos <= 0) {
+      // Scroll to the bottom when you reach the top
+      setScrollPos(scrollHeight - clonesHeight);
+      disableScroll = true;
+    }
+  }
+
+  if (disableScroll) {
+    // Disable scroll-jumping for a short time to avoid flickering
+    window.setTimeout(function () {
+      disableScroll = false;
+    }, 40);
+  }
+}
+
+window.requestAnimationFrame(reCalc);
+
+context.addEventListener('scroll', function () {
+  window.requestAnimationFrame(scrollUpdate);
+}, false);
+
+window.addEventListener('resize', function () {
+  window.requestAnimationFrame(reCalc);
+}, false);
+
+// Just for this demo: Center the middle block on page load
+window.onload = function () {
+  setScrollPos(2)
+};
 
 
