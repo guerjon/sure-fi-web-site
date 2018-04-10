6b2321b81af1e0abbe7ca9f9064819f54c6242fb
Jonathan Guerrero <guerjon@gmail.com>
2018-04-10 09:54:42 -0600

    testing

---
+++ js/where_to_buy.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.vertical-carousel').slick({
+    vertical: true,
+    slidesToShow: 3,
+    touchMove: true,
+    verticalSwiping: true,
+    arrows: false
+  });
+});
+
+
