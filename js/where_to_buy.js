e243ee9e648c608550b8635db60639aaf1d21fc5
Jonathan Guerrero <guerjon@gmail.com>
2018-04-11 08:42:37 -0600

    partners thing

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -9,6 +9,26 @@ var doc = window.document,
   i = 0;
 
 
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
 function getScrollPos () {
   return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
 }
@@ -68,8 +88,12 @@ function scrollUpdate () {
 window.requestAnimationFrame(reCalc);
 
 context.addEventListener('scroll', function () {
+  if(getMobileOperatingSystem() == "Android"){
+    window.requestAnimationFrame(scrollUpdate);  
+  }else{
+    clones.remove()
+  }
   
-  window.requestAnimationFrame(scrollUpdate);
 
 }, false);
 
