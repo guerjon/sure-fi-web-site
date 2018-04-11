9a80a014f334bfbdcb0e2125f316b3f8300decb3
Jonathan Guerrero <guerjon@gmail.com>
2018-04-11 13:26:40 -0600

    apple dumb

--- js/where_to_buy.js
+++ js/where_to_buy.js
@@ -38,16 +38,6 @@ function setScrollPos (pos) {
   context.scrollTop = pos;
 }
 
-function getClonesHeight () {
-  clonesHeight = 0;
-
-  for (i = 0; i < clones.length; i += 1) {
-    clonesHeight = clonesHeight + clones[i].offsetHeight;
-  }
-
-  return clonesHeight;
-}
-
 function reCalc () {
   scrollPos = getScrollPos();
   scrollHeight = context.scrollHeight;
@@ -84,47 +74,69 @@ function scrollUpdate () {
 }
 
 function scrollIOSUpdate(){
+  console.log("scrollIOSUpdate",scrollPos,scrollHeight)
     scrollPos = getScrollPos();
 
-    if (clonesHeight + scrollPos >= scrollHeight) {
-      // Scroll to the top when you’ve reached the bottom
-      setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
-      $("#logic").text("Touch the bottom")
-    } else if (scrollPos <= 0) {
+    if (scrollPos > scrollHeight -100) {
       
+      var elements = getElements()
+      for(var i = elements.length; i >= 0; i--){
+          var element = elements[i]
+          context.appendChild(element)
+      }
+
+    } else if (scrollPos <= 0) {
       setScrollPos(scrollHeight - clonesHeight);
     }
 }
 
-window.requestAnimationFrame(reCalc);
+function getClonesHeight () {
+  clonesHeight = 0;
+  for (i = 0; i < clones.length; i += 1) {
+    clonesHeight = clonesHeight + clones[i].offsetHeight;
+  }
+  return clonesHeight;
+}
 
 function init(){
   var os = getMobileOperatingSystem()
   if(os == "iOS")  {
     handleIOS()
   }else{
+    window.requestAnimationFrame(reCalc);
     handleAndroid()
   }
 }
 function handleIOS(){
-  setInterval(function() {
-    if ( didScroll ) {
-        window.requestAnimationFrame(scrollIOSUpdate)    
-        didScroll = false;
-    }
-  }, 650);
+  console.log("handleIOS()")
+  var clonesHeight = getClonesHeight()
+  deleteClones()
+  console.log("clonesHeight",clonesHeight)
+  console.log("scrollHeight",context.scrollHeight)
+
+  scrollHeight = context.scrollHeight - clonesHeight;
+
+  context.addEventListener('scroll', function (pos) {    
+    window.requestAnimationFrame(scrollIOSUpdate)
+  })
+}
+
+function deleteClones(){
+  $('.is-clone').remove()
+}
+
+function getElements(){
+  var elements = $(".no-clone")
+  var clone_elements = []
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var element = elements[i].cloneNode(true)
+    clone_elements.push(element)
+  }
+  return clone_elements
 }
 
 function handleAndroid(){
-    context.onscroll = function() {
-      if(getScrollPos() > 1){
-        didScroll = true;  
-      }
-    };
-  
     context.addEventListener('scroll', function (pos) {  
-      logic = logic + 1
-      $("#logic").text(logic)
       window.requestAnimationFrame(scrollUpdate);  
     }, false);        
 }
