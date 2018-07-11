36e237047ddf207c0a97de6f467cb7d0e81b36ad
Jonathan Guerrero <guerjon@gmail.com>
2018-07-11 15:22:42 -0600

    added changes

--- js/creative.js
+++ js/creative.js
@@ -124,19 +124,5 @@ function shuffle(array) {
       owl.trigger('prev.owl.carousel', [300]);
   })
 
-  $(".dropdown-toggle-products").click(function(event) {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-
-      if(width > 992){ // the nav bar is shrunken
-        let location_array = window.location.pathname.split("/")
-        console.log()
-        location_array[location_array.length - 1] = "products.html"
-        const result = location_array.reduce((acomulator,current_value) => acomulator + "/" + current_value )
-        window.location = result
-      }
-
-  })
-
 })(jQuery); // End of use strict
 
