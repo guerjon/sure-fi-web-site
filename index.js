7c65dc8c7ea723755d1ccd081ad20c1c37d247b5
Jonathan Guerrero <guerjon@gmail.com>
2018-07-19 14:45:10 -0600

    more changes

--- index.js
+++ index.js
@@ -22,45 +22,137 @@ var Fotter = function (_React$Component) {
   _createClass(Fotter, [{
     key: "render",
     value: function render() {
-
+      var tel = "tel:1-844-787-3340";
+      var mailto = "mailto:support@sure-fi.com";
       return React.createElement(
-        "section",
-        { style: { background: "black" } },
+        "div",
+        null,
         React.createElement(
-          "div",
-          { className: "container" },
+          "section",
+          { id: "contact", style: { marginTop: 10 } },
           React.createElement(
             "div",
-            { className: "row" },
+            { className: "container" },
             React.createElement(
               "div",
-              { className: "col-6 col-lg-6" },
+              { className: "row", style: { textAlign: "center" } },
               React.createElement(
-                "a",
-                { href: "https://itunes.apple.com/us/app/sure-fi/id1259963492?mt=8", target: "_blank", style: { float: "right" } },
-                React.createElement("img", { src: "images/app_store.png", className: "img-fluid" })
+                "div",
+                { className: "col-lg-12" },
+                React.createElement(
+                  "h1",
+                  null,
+                  "Contact"
+                ),
+                React.createElement("hr", null)
               )
             ),
             React.createElement(
               "div",
-              { className: "col-6 col-lg-6" },
+              { className: "row" },
               React.createElement(
-                "a",
-                { href: "https://play.google.com/store/apps/details?id=com.surefi&hl=en", target: "_blank" },
-                React.createElement("img", { src: "images/play_store.png", className: "img-fluid" })
+                "div",
+                { className: "col-lg-12 mx-auto text-center" },
+                React.createElement("img", { src: "images/hall-labs.jpg", alt: "hall-labls-image", style: { borderTopTightRadius: 5, borderTopLeftRadius: 5, width: "100%" } }),
+                React.createElement(
+                  "div",
+                  { id: "hall-bottom-foot" },
+                  React.createElement(
+                    "div",
+                    { style: { width: "20%", float: "right", marginTop: 20, marginRight: 20 } },
+                    React.createElement("img", { src: "http://halllabs.com/wp-content/uploads/2017/06/HallLabs-Logo-2.png", className: "img-responsive", style: { marginTop: "5%", marginBottom: "5%", width: "100%" } })
+                  )
+                ),
+                React.createElement(
+                  "div",
+                  { id: "contact_text_container", style: { textAlign: "left" } },
+                  React.createElement(
+                    "div",
+                    { style: { width: "50%", float: "left", fontSize: "1rem" } },
+                    React.createElement(
+                      "p",
+                      { className: "contact-font" },
+                      "Contact : Sure-Fi Inc."
+                    ),
+                    React.createElement(
+                      "p",
+                      { className: "contact-font" },
+                      "3000 SIERRA VISTA WAY"
+                    ),
+                    React.createElement(
+                      "p",
+                      { className: "contact-font" },
+                      "PROVO, UT 84606"
+                    )
+                  ),
+                  React.createElement(
+                    "div",
+                    { style: { width: "50%", float: "left", fontSize: "1rem", textAlign: "right" } },
+                    React.createElement(
+                      "a",
+                      { href: mailto, target: "_top", style: { textDecoration: "none", color: "black" } },
+                      React.createElement(
+                        "p",
+                        { className: "contact-font" },
+                        "support@sure-fi.com ",
+                        React.createElement("i", { style: { fontSize: "1rem" }, className: "fa fa-envelope-o fa-3x mb-1 sr-contact" })
+                      )
+                    ),
+                    React.createElement(
+                      "a",
+                      { href: tel },
+                      React.createElement(
+                        "p",
+                        { className: "contact-font" },
+                        "1-844-787-3340",
+                        React.createElement("i", { style: { fontSize: "1rem", color: "green" }, className: "fa fa-phone fa-3x mb-3 sr-contact" })
+                      )
+                    )
+                  )
+                )
               )
             )
-          ),
+          )
+        ),
+        React.createElement(
+          "section",
+          { style: { background: "black" } },
           React.createElement(
             "div",
-            { className: "row" },
+            { className: "container" },
             React.createElement(
               "div",
-              { className: "col col-lg-12", style: { textAlign: "center", padding: 60 } },
+              { className: "row" },
               React.createElement(
-                "h4",
-                { style: { color: "white" } },
-                "Sure-Fi Inc. \xA9 2017"
+                "div",
+                { className: "col-6 col-lg-6" },
+                React.createElement(
+                  "a",
+                  { href: "https://itunes.apple.com/us/app/sure-fi/id1259963492?mt=8", target: "_blank", style: { float: "right" } },
+                  React.createElement("img", { src: "images/app_store.png", className: "img-fluid" })
+                )
+              ),
+              React.createElement(
+                "div",
+                { className: "col-6 col-lg-6" },
+                React.createElement(
+                  "a",
+                  { href: "https://play.google.com/store/apps/details?id=com.surefi&hl=en", target: "_blank" },
+                  React.createElement("img", { src: "images/play_store.png", className: "img-fluid" })
+                )
+              )
+            ),
+            React.createElement(
+              "div",
+              { className: "row" },
+              React.createElement(
+                "div",
+                { className: "col col-lg-12", style: { textAlign: "center", padding: 60 } },
+                React.createElement(
+                  "h4",
+                  { style: { color: "white" } },
+                  "Sure-Fi Inc. \xA9 2017"
+                )
               )
             )
           )
--- src/index.js
+++ src/index.js
@@ -10,31 +10,80 @@ class Fotter extends React.Component {
   }
 
   render() {
-
+    const tel = "tel:1-844-787-3340";
+    const mailto = "mailto:support@sure-fi.com"
     return (
-      <section style={{background: "black"}}>
-        <div className="container">      
-          <div className="row" >
-            <div className="col-6 col-lg-6" >
-            <a href="https://itunes.apple.com/us/app/sure-fi/id1259963492?mt=8" target="_blank" style={{float: "right"}}>
-              <img src="images/app_store.png" className="img-fluid" />  
-            </a>                                          
+      <div>
+        <section id="contact" style={{marginTop: 10}}>
+            <div className="container">
+              <div className="row" style={{textAlign: "center"}}>
+                <div className="col-lg-12">
+                  <h1>Contact</h1>    
+                  <hr/>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-12 mx-auto text-center">
+                 <img src="images/hall-labs.jpg" alt="hall-labls-image" style={{borderTopTightRadius: 5,borderTopLeftRadius: 5,width: "100%"}}/> 
+                  <div id="hall-bottom-foot">
+                    <div style={{width: "20%",float: "right",marginTop: 20,marginRight: 20}}>
+                      <img src="http://halllabs.com/wp-content/uploads/2017/06/HallLabs-Logo-2.png" className="img-responsive" style={{marginTop: "5%",marginBottom: "5%",width: "100%"}}/> 
+                    </div>
+                  </div>
+                  <div id="contact_text_container" style={{textAlign: "left"}}>
+                    <div style={{width: "50%",float: "left",fontSize: "1rem"}}>
+                      <p className="contact-font">
+                        Contact : Sure-Fi Inc.
+                      </p>
+                      <p className="contact-font">
+                        3000 SIERRA VISTA WAY  
+                      </p>
+                      <p className="contact-font">
+                        PROVO, UT 84606  
+                      </p>
+                    </div>
+                    <div style={{width:"50%",float: "left",fontSize: "1rem",textAlign: "right"}}>
+                      <a href={mailto} target="_top" style={{textDecoration:"none",color: "black"}}>
+                        <p className="contact-font">
+                          support@sure-fi.com <i style={{fontSize: "1rem"}} className="fa fa-envelope-o fa-3x mb-1 sr-contact"></i>
+                        </p>
+                      </a>
+                      <a href={tel}>
+                        <p className="contact-font">
+                          1-844-787-3340  
+                          <i style={{fontSize: "1rem",color: "green"}} className="fa fa-phone fa-3x mb-3 sr-contact"></i>
+                        </p>
+                      </a>
+                    </div>              
+                  </div>                      
+                </div>
+              </div>        
             </div>
-            <div className="col-6 col-lg-6">
-            <a href="https://play.google.com/store/apps/details?id=com.surefi&hl=en" target="_blank" >
-              <img src="images/play_store.png" className="img-fluid" />
-            </a>                                          
+          </section>      
+          <section style={{background: "black"}}>
+            <div className="container">      
+              <div className="row" >
+                <div className="col-6 col-lg-6" >
+                <a href="https://itunes.apple.com/us/app/sure-fi/id1259963492?mt=8" target="_blank" style={{float: "right"}}>
+                  <img src="images/app_store.png" className="img-fluid" />  
+                </a>                                          
+                </div>
+                <div className="col-6 col-lg-6">
+                <a href="https://play.google.com/store/apps/details?id=com.surefi&hl=en" target="_blank" >
+                  <img src="images/play_store.png" className="img-fluid" />
+                </a>                                          
+                </div>
+              </div>
+              <div className="row">
+                <div className="col col-lg-12" style={{textAlign: "center",padding: 60}}>
+                <h4 style={{color: "white"}}>
+                Sure-Fi Inc. © 2017
+                </h4>                  
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col col-lg-12" style={{textAlign: "center",padding: 60}}>
-            <h4 style={{color: "white"}}>
-            Sure-Fi Inc. © 2017
-            </h4>                  
-            </div>
-          </div>
-        </div>
-      </section>
+          </section>
+      </div>
     );
   }
 }
@@ -43,3 +92,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const domContainer = document.querySelector('#footer');
   ReactDOM.render(e(Fotter), domContainer);
 });
+
+
