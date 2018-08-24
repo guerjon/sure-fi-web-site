c87d244613be39661837ca16f3f4c579b8cad7a0
Jonathan Guerrero <guerjon@gmail.com>
2018-08-24 13:53:03 -0700

    buy this product now

---
+++ js/wiegand-controller.js
@@ -0,0 +1,16 @@
+/*
+function animateThisProduct(){
+	console.log("animate")
+	$(".buy-this-product a").addClass('animated bounce');
+}
+
+
+$(function() {
+	var interval = 0;
+
+    if(interval == 0){
+
+    	interval = setInterval(2000,() => animateThisProduct());
+    }    
+
+});*/
